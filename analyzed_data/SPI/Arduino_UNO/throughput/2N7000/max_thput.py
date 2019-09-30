776997b2ec24aea0f2573fe38d75427cc20741b6
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2019-09-30 23:54:19 +0900

    Added max_thput.py

---
+++ analyzed_data/SPI/Arduino_UNO/throughput/2N7000/max_thput.py
@@ -0,0 +1,20 @@
+import os
+
+with open(os.path.abspath("throughput.txt"), mode = 'r') as f:
+  thput_txt = f.readlines()
+  thput_txt.pop(0)
+
+max_throughput = 0
+
+for line in thput_txt:
+  elms = line.split()
+
+  if int(elms[1]) != 1024:
+    continue
+
+  thput = float(elms[3]) * (1 - float(elms[8]))
+
+  if thput > max_throughput:
+    max_throughput = thput
+
+print("Maximum throughput(only by correct transactions) is {}[bps]".format(max_throughput))
